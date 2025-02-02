5cb254a1a76ead7c79ae87198abc99f75d9a5ddb
navid-kamal <navidkamal568@gmail.com>
2025-02-02 18:50:17 +0600

    feat: initialize Next.js 15 project with TypeScript and Tailwind CSS setup

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold text-blue-500 text-center mt-8">
+      Next.js 15 + TypeScript + Tailwind CSS 🚀
+    </h1>
+  );
+}
